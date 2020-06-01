7ea9af3988502587bd53ea04ef333ab93e0cfd1a
Gabriel Rufino <contato@gabrielrufino.com>
2020-05-31 23:08:17 -0300

    First commit

---
+++ src/screens/Auth.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Auth() {
+  return (
+    <div>
+      <div className="tabs">
+        <ul>
+          <li className="is-active"><a>Login</a></li>
+          <li><a>Register</a></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Auth
