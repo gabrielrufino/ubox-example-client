8411f3bce80d1ff27893b9fdc2d5f91d09ed0602
Gabriel Rufino <contato@gabrielrufino.com>
2020-06-07 20:52:55 -0300

    Remove build from .gitignore to host in github

---
+++ build/precache-manifest.9d9033df6ba5d8cdcfe5daf5bceebcc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d51f93dcf1f435cdaf469058d8056375",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c185cdcc75d595b7af9",
+    "url": "/static/css/2.240f97b7.chunk.css"
+  },
+  {
+    "revision": "0c185cdcc75d595b7af9",
+    "url": "/static/js/2.dfe039ea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dfe039ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6553e066f9d4c0f9a5d",
+    "url": "/static/js/main.109b0888.chunk.js"
+  },
+  {
+    "revision": "d8412d89afb61b660a1c",
+    "url": "/static/js/runtime-main.57174c6f.js"
+  }
+]);
