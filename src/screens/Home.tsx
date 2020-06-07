8aea1db305b6d565db663fcb89b3104dcaa76ff9
Gabriel Rufino <contato@gabrielrufino.com>
2020-06-07 20:48:24 -0300

    Initial login and register

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 import { Link } from 'react-router-dom'
 
-function Auth() {
+function Home() {
   return (
     <div>
       <Link to="/login">
         <button className="button is-primary is-medium is-fullwidth">Login</button>
       </Link>
+      <div className="mb-4" />
       <Link to="/register">
         <button className="button is-link is-medium is-fullwidth">Register</button>
       </Link>
@@ -15,4 +16,4 @@ function Auth() {
   )
 }
 
-export default Auth
+export default Home
