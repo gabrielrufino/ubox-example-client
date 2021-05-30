fa372e3dfc7251287fb9fb9d10eada6a88509ddb
Gabriel Rufino <contato@gabrielrufino.com>
2021-05-30 20:15:07 -0300

    Requesting users

--- src/screens/Users.tsx
+++ src/screens/Users.tsx
@@ -1,7 +1,26 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
+
+interface IUser {
+  name: string;
+}
 
 function Users() {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState<IUser[]>([])
+
+  useEffect(function () {
+    const headers = new Headers()
+    headers.append('Content-Type', 'application/json')
+    headers.append('Authorization', localStorage.getItem('token') || '')
+
+    fetch(`${process.env.REACT_APP_API_URL}/api/users`, {
+      headers
+    })
+      .then(response => response.json())
+      .then(({ data }) => {
+        setUsers(data);
+      })
+
+  }, [])
 
   return (
     <div>
